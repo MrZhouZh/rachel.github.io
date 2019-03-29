70048692142777ca311898669913adbe09379f59
zhouzk <1204634123@qq.com>
2019-03-29 13:50:28 +0800

    del test

--- static/js/mallMonitor.js
+++
@@ -1,3 +0,0 @@
-!(function(c,b,d,a){c[a]||(c[a]={});c[a].config={pid:"9zrmbpxfoe@06a1def531143cc",appType:undefined,imgUrl:"https://arms-retcode.aliyuncs.com/r.png?",sendResource:true};
-with(b)with(body)with(insertBefore(createElement("script"),firstChild))setAttribute("crossorigin","",src=d)
-})(window,document,"https://retcode.alicdn.com/retcode/bl.js","__bl");
--- static/js/storeMonitor.js
+++
@@ -1,3 +0,0 @@
-!(function(c,b,d,a){c[a]||(c[a]={});c[a].config={pid:"9zrmbpxfoe@26c3abf02a28083",imgUrl:"https://arms-retcode.aliyuncs.com/r.png?",sendResource:true,useFmp:true,enableSPA:true};
-with(b)with(body)with(insertBefore(createElement("script"),firstChild))setAttribute("crossorigin","",src=d)
-})(window,document,"https://retcode.alicdn.com/retcode/bl.js","__bl");
